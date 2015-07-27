086deed545ee183d9d231b0d1cbd4218ca3eb10b
DimaSanKiev <dmytro.burdyga@gmail.com>
2015-07-27 23:54:12 +0300

    test Hibernate

---
+++ src/main/java/ua/burdyga/hibernate/HibernateTest.java
@@ -0,0 +1,22 @@
+package ua.burdyga.hibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import ua.burdyga.dto.UserDetails;
+
+public class HibernateTest {
+
+    public static void main(String[] args) {
+        UserDetails user = new UserDetails();
+        user.setUserId(1);
+        user.setUserName("FirstUser");
+
+        SessionFactory factory = new Configuration().configure().buildSessionFactory();
+        Session session = factory.openSession();
+        session.beginTransaction();
+        session.save(user);
+        session.getTransaction().commit();
+    }
+
+}
