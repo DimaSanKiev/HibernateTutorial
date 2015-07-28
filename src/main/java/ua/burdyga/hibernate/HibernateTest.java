7d44ae97ca131e82131a430044b0caac685731f4
DimaSanKiev <dmytro.burdyga@gmail.com>
2015-07-28 14:39:27 +0300

    add more annotations

--- src/main/java/ua/burdyga/hibernate/HibernateTest.java
+++ src/main/java/ua/burdyga/hibernate/HibernateTest.java
@@ -5,12 +5,17 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import ua.burdyga.dto.UserDetails;
 
+import java.util.Date;
+
 public class HibernateTest {
 
     public static void main(String[] args) {
         UserDetails user = new UserDetails();
-        user.setUserId(2);
-        user.setUserName("SecondUser");
+        user.setUserId(1);
+        user.setUserName("First User");
+        user.setAddress("First User's address");
+        user.setJoinedDate(new Date());
+        user.setDescription("Description one");
 
         SessionFactory factory = new Configuration().configure().buildSessionFactory();
         Session session = factory.openSession();
