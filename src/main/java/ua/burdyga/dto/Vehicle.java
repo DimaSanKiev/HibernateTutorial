59faf640781aad05973dd1d2f000fed2d8a0d812
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2015-09-08 00:17:44 +0300

    Implementing Instance with Joined Strategy

--- src/main/java/ua/burdyga/dto/Vehicle.java
+++ src/main/java/ua/burdyga/dto/Vehicle.java
@@ -3,7 +3,7 @@ package ua.burdyga.dto;
 import javax.persistence.*;
 
 @Entity
-@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@Inheritance(strategy = InheritanceType.JOINED)
 public class Vehicle {
     @Id
     @GeneratedValue
