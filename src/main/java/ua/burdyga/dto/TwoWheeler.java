3a897abe9f480978e1a062c03129002a1dc38705
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2015-09-07 17:01:28 +0300

    Implementing Inheritance with Table Per Class Strategy

--- src/main/java/ua/burdyga/dto/TwoWheeler.java
+++ src/main/java/ua/burdyga/dto/TwoWheeler.java
@@ -1,10 +1,8 @@
 package ua.burdyga.dto;
 
-import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
 @Entity
-@DiscriminatorValue("Bike")
 public class TwoWheeler extends Vehicle {
     private String steeringHandle;
 
--- src/main/java/ua/burdyga/dto/Vehicle.java
+++ src/main/java/ua/burdyga/dto/Vehicle.java
@@ -3,9 +3,7 @@ package ua.burdyga.dto;
 import javax.persistence.*;
 
 @Entity
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-@DiscriminatorColumn(name = "VEHICLE_TYPE",
-        discriminatorType = DiscriminatorType.STRING)
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public class Vehicle {
     @Id
     @GeneratedValue
